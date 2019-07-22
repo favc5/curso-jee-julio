51254f434cf5e06d73feb5af899d36e74dc9d570
Fabian Valdes <fabian.valdes@ibm.com>
2019-07-22 14:05:46 -0400

    se actualiza mensajes a desplegar por pantalla

--- curso-jee-sesion1/src/com/ibm/curso/jee/StartApp.java
+++ curso-jee-sesion1/src/com/ibm/curso/jee/StartApp.java
@@ -5,6 +5,7 @@ public class StartApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("primera version");
+		System.out.println("hello world!");
 	}
 
 }
