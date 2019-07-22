93500f6f63dbce183877a63f4ec1dcb113085ebf
Fabian Valdes <fabian.valdes@ibm.com>
2019-07-22 14:01:31 -0400

    linea base

---
+++ curso-jee-sesion1/src/com/ibm/curso/jee/StartApp.java
@@ -0,0 +1,10 @@
+package com.ibm.curso.jee;
+
+public class StartApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("primera version");
+	}
+
+}
